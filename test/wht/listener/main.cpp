3cb97cc340caf0cd592b02ac4a1ef5d55ce35ffe
Sergey Belov <serge_belov@ukr.net>
2020-06-24 19:29:03 +0300

    CTest. Skeleton finished.

--- test/wht/listener/main.cpp
+++ test/wht/listener/main.cpp
@@ -1,3 +1,22 @@
-int main() {
+#include <iostream>
+
+typedef int (*UnitTestProc)();
+
+static int UnitTest1() {
   return 1;
+}
+
+static int UnitTest2() {
+  return 1;
+}
+
+static const UnitTestProc UNIT_TESTS[] = {nullptr, UnitTest1, UnitTest2};
+
+int main(int argc, const char* argv[]) {
+  if (argc < 2) {
+    std::cout << "Error! No test case number in command line arg!";
+    return 1;
+  }
+
+  return UNIT_TESTS[atoi(argv[1])]();
 }
